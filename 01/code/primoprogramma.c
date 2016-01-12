366dcea1c953a4a006a5a3468d22e92e797bb930
Danilo Pianini <danilo.pianini@unibo.it>
2016-01-12 15:49:19 +0100

    Add first lesson code and .gitignore

---
+++ 01/code/primoprogramma.c
@@ -0,0 +1,21 @@
+
+int main(void)
+{
+    int var;
+    double var2;
+    var2 = 7.0 / 3;
+//    printf("ciao\n %d, %f\n\n", 3, 3);
+    var = 5 < 3; // assegna a 0 perché 0 = falso
+    printf("Var2 vale: %f\nVar vale: %d\n", var2, var);
+    /*
+     * { }} {} }{} f dskmrejns
+     * 
+     * 
+     */
+/*    if (var) { // Questo è un if.
+        printf("Piu di tre\n");
+    } else {
+        printf("Meno di tre\n");
+    }
+    printf("Ho finito\n");*/
+}
